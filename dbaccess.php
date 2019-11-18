3c1ce796e5c615c68252d8a8f4b5f80f92947a49
haradashinji <harada.shinji@hyron.co.jp>
2019-11-18 10:47:54 +0900

    retry3

--- dbaccess.php
+++ dbaccess.php
@@ -42,7 +42,7 @@ Last Access Time<br><br>
 <?php foreach ($rec as $a):?>
         <?=escape1($a)?><br>
 <?php endforeach; ?>
-Add comment by Shinji Harada
+Add comment by Mamoru Ito
 </body>
 </html>
 
