b4d16f9fa6a2245d896f7324733ddb593189c316
haradashinji <harada.shinji@hyron.co.jp>
2019-11-18 10:40:45 +0900

    retry2

---
+++ hello.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+  echo '<p>Hellow World</p>';
+  echo 'now : '.date("Y/m/d H:i:s");
+  echo "<br />";
+  echo "<br />";
+ ?>
+ <?php
+  echo 'Hellow World';
+  echo "<br />";
+  echo "<br />";
+  echo 'now : ';
+  echo date("Y/m/d H:i:s");
+ ?>
+ </body>
+</html>
